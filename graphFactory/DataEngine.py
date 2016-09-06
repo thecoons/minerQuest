cf064cd7797317ae55be4fc470ca29e990ee719c
thecoons <antobarth@gmail.com>
2016-09-06 19:42:14 +0200

    fonction de génération de graph connexes

--- graphFactory/DataEngine.py
+++ graphFactory/DataEngine.py
@@ -1,16 +1,18 @@
 ###
 #Class : DataEngine
 #Attributs:
-#   ~
+#   engine_type : str
+#   arr_labels : str[]
 #Methodes:
 #   elementGenerator()
 ###
 import networkx as nx
+import numpy as np
 
 class DataEngine:
     def __init__(self):
         self._engine_type = ''
-
+        self._arr_labels = []
     @property
     def engine_type(self):
         return self._engine_type
@@ -19,15 +21,37 @@ class DataEngine:
     def engine_type(self,name):
         self._engine_type = name
 
+    @property
+    def _arr_labels(self):
+        return self.__arr_labels
+
+    @_arr_labels.setter
+    def _arr_labels(self,arr_labels):
+        self.__arr_labels = arr_labels
+
+
 class GraphConnexBinEngine(DataEngine):
     def __init__(self):
         self._engine_type = 'GraphConnexBin'
+        self._arr_labels = ['True','False']
 
     def generate_element(self,n,p,value):
         if(value):
             G = nx.gnp_random_graph(n,p)
-            if(nx.number_connected_components(G)>1):
+            ##Test
+            print("nbr_comp_1:"+str(nx.number_connected_components(G)))
+            if(nx.number_connected_components(G)!=1):
                 return self.generate_element(n,p,value)
             else:
                 return (G, value)
-            
+        else:
+            p1 = np.random.random_integers((n/2)-(n*0.1),(n/2)+(n*0.1))
+            g_1 = nx.gnp_random_graph(p1,p)
+            g_2 = nx.gnp_random_graph(n-p1,p)
+            G = nx.disjoint_union(g_1,g_2)
+            ##Test
+            print("nbr_comp_2:"+str(nx.number_connected_components(G)))
+            if(nx.number_connected_components(G)!=2):
+                return self.generate_element(n,p,value)
+            else:
+                return (G, value)
--- visualisationGraph/utils.py
+++ visualisationGraph/utils.py
@@ -4,9 +4,10 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import networkx as nx
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 
+from networkx.drawing.nx_agraph import graphviz_layout
 from scipy import stats
 from scipy.stats import kendalltau
 
@@ -15,3 +16,22 @@ def gridVisualisationData(input_data,type_graph):
     g.map_diag(plt.hist)
     g.map_offdiag(plt.scatter)
     g.savefig("Datarows/"+type_graph+"/datarows_grid.png")
+    plt.clf()
+
+# Export un graph et indique sa planarité#
+def exportGraph(mat, name, path=''):
+    G = nx.from_numpy_matrix(mat)
+    pos = graphviz_layout(G)
+    nx.draw(G,pos)
+    plt.savefig(path+'img_'+name+'.png')
+    plt.clf()
+
+def exportAccuracyLearning(train_acu,valid_acu,x_range,path=''):
+    plt.plot(x_range, train_acu,'-b', label='Training')
+    plt.plot(x_range, valid_acu,'-g', label='Validation')
+    plt.legend(loc='lower right', frameon=False)
+    plt.ylim(ymax = 1.1, ymin = 0.7)
+    plt.ylabel('accuracy')
+    plt.xlabel('step')
+    plt.savefig('Datarows/'+path+'/accuracyTrainingValidation.png')
+    plt.clf()
